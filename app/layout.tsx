09e322078529880cd19f72602e585565bbcc7376
Bautista <bautistamarinelliscaglia@gmail.com>
2026-02-20 19:41:11 -0300

    Agregado Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter, Geist_Mono } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/next"
 
 const _inter = Inter({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
